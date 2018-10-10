358eeecf0315cea67c9bf5f64138cb6c20083337
wangss <genhaoai@gmail.com>
2018-10-10 22:34:42 +0800

    Initial commit

---
+++ src/components/InfoList.vue
@@ -0,0 +1,69 @@
+<template>
+<div>
+  <!--https://wangdahoo.github.io/vue-scroller/#/-->
+  <scroller :on-refresh="refreshIndex">
+    <panel header="小区实时信息" :list="list" type="5" @on-click-item="itemClick"
+           ></panel>
+  </scroller>
+
+</div>
+</template>
+
+<script>
+  import {Panel} from 'vux'
+
+  export default {
+    name: 'InfoList',
+    components: {Panel},
+    data () {
+      return {
+        footer: {title: '加载更多...'},
+        list: [{
+          src: 'http://somedomain.somdomain/x.jpg',
+          fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+          title: '标题一',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+          url: '/component/cell'
+        }, {
+          src: 'http://somedomain.somdomain/x.jpg',
+          fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+          title: '标题一',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+          url: '/component/cell'
+        }, {
+          src: 'http://somedomain.somdomain/x.jpg',
+          fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+          title: '标题一',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+          url: '/component/cell'
+        }, {
+          src: 'http://somedomain.somdomain/x.jpg',
+          fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+          title: '标题一',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+          url: '/component/cell'
+        }, {
+          src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+          title: '标题二',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+          meta: {
+            source: '来源信息',
+            date: '时间',
+            other: '其他信息'
+          }
+        }]
+      }
+    },
+    methods: {
+      itemClick  (item) {
+        console.info(item.title)
+      },
+      refreshIndex (done) {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SendInfo.vue
@@ -0,0 +1,72 @@
+<template>
+
+  <div>
+    <br>
+    <x-header>Melp</x-header>
+    <br>
+    <group title="发布信息">
+      <x-input title="姓名" placeholder="输入姓名" v-model="name"></x-input>
+      <x-input title="电话" placeholder="输入手机号" v-model="tel"></x-input>
+      <datetime v-model="startDate" format="YYYY-MM-DD HH:mm"
+                :minute-list="['00', '15', '30', '45']" @on-change="change" title="起始时间"></datetime>
+      <datetime v-model="endDate" format="YYYY-MM-DD HH:mm"
+                :minute-list="['00', '15', '30', '45']" @on-change="change" title="失效时间"></datetime>
+
+      <x-textarea title="消息内容" v-model="info" :max=30  placeholder="发布消息内容"></x-textarea>
+
+      <flexbox>
+        <flexbox-item>
+          <x-button type="primary" @click.native="sendInfo">发布</x-button>
+        </flexbox-item>
+        <flexbox-item>
+          <x-button type="warn" @click.native="resetInfo">重置</x-button>
+        </flexbox-item>
+      </flexbox>
+      <toast v-model="showPositionValue" type="text"
+             :time="800" is-show-mask text="发布成功" position="middle">发布成功1</toast>
+    </group>
+  </div>
+</template>
+
+<script>
+  import {XInput, Group, Datetime, XTextarea, XButton, Flexbox, FlexboxItem, Toast, XHeader} from 'vux'
+
+  export default {
+    name: 'SendInfo',
+    components: {XInput, Group, Datetime, XTextarea, XButton, Flexbox, FlexboxItem, Toast, XHeader},
+    data () {
+      return {
+        name: '',
+        tel: '',
+        startDate: '',
+        endDate: '',
+        info: '',
+        length: 30,
+        showPositionValue: false
+      }
+    },
+    methods: {
+      change: function () {
+      },
+      resetInfo: function () {
+        this.info = ''
+      },
+      sendInfo: function () {
+        this.showPositionValue = true
+        /*this.$vux.toast.show({
+          text: '发布成功2',
+          onShow () {
+            console.log('Plugin: I\'m showing')
+          },
+          onHide () {
+            console.log('Plugin: I\'m hiding')
+          }
+        })*/
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
